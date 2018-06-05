8ae937f764c1c0777b9414057edfe3ee69b2752d
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2018-06-05 17:13:19 +0200

    Results page
    Details
    . First impressions on building the user interface (unfinished).

--- src/components/ResultsPage/index.js
+++ src/components/ResultsPage/index.js
@@ -10,7 +10,19 @@ class ResultsPage extends Component {
         <NavigationBar />
 
         <div className='results-page'>
+          <p className='title offset-below'>Resultados da temporada</p>
 
+          <div className='action-buttons'>
+            <button className='select-season'>
+              <div className='common-area'>
+                <p className='text'>Maio 2018</p>
+              </div>
+
+              <span className='faded-area'>
+                <span className='icon' />
+              </span>
+            </button>
+          </div>
         </div>
       </Fragment>
     );
