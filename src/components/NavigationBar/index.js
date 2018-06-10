a6b968066f645c663e18956e0d275e5533d8c41a
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2018-06-10 13:39:21 +0200

    NavigationBar - Menu toggling
    Details
    . Added a menu toggling functionality to switch between a "open" and "closed" state.

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -3,15 +3,37 @@ import React, { Component } from 'react';
 import './index.styles.css';
 
 class NavigationBar extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggleMenu() {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen,
+    }));
+  }
+
   render() {
     return (
-      <header className='navigation-bar'>
+      <header className={`navigation-bar
+        ${this.state.isOpen ? '--menu-is-open' : ''}
+      `.trim()}>
         <div className='title'>
           <p className='text'># fifa-champs</p>
         </div>
 
-        <div className='menu'>
-          <button className='button'/>
+        <div className='menu-container'>
+          <div className='menu'
+            onClick={() => this.toggleMenu()}
+          >
+            <span className='text'>Menu</span>
+
+            <div className='icon'>
+              <div/>
+              <div/>
+              <div/>
+            </div>
+          </div>
         </div>
       </header>
     );
