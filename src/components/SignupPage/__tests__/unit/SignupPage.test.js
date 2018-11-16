dc261b7b250e7366088935aa97f4a529f39fe449
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2018-11-16 14:17:07 +0100

    WIP: Test cases for the "SignupPage" presentational component
    Details
    . Changed the test approaches from "unit code testing" to "functional requirements" testing (as if I would describe the feature for you if I were a product manager).

--- src/components/SignupPage/__tests__/unit/SignupPage.test.js
+++ src/components/SignupPage/__tests__/unit/SignupPage.test.js
@@ -1,6 +1,113 @@
 describe('[component] SignupPage', () => {
   describe('initial render', () => {
+    it('must show the "signup form", hiding the "error"/"successful" pages', () => {
 
+    });
+
+    it('all "fields" must have no value or errors', () => {
+
+    });
+
+    it('the "action button" must be disabled', () => {
+
+    });
+
+    it('the "action button" must not show the loading spinner', () => {
+
+    });
+  });
+
+  describe('general functional requirements for "fields"', () => {
+    const FIELDS = ['email', 'username', 'password', 'confirmPassword'];
+
+    FIELDS.forEach(field => {
+      it(`must contain an "field" for setting "${field}"`, () => {
+
+      });
+
+      it(`must set value to "${field}" field when focusing and typing on them`, () => {
+
+      });
+
+      it(`must validate "${field}" field after focusing, typing and leaving it`, () => {
+
+      });
+    });
+  });
+
+  describe('functional requirements for "action button"', () => {
+    it('must exist in order to be able to submit the "signup form"', () => {
+
+    });
+
+    it('must be enabled when providing valid data for all fields', () => {
+
+    });
+
+    it('must be disabled while the form has errors', () => {
+
+    });
+  });
+
+  describe('functional requirements for "confirm password" field', () => {
+    it('must display an error when typing an value and removing it completely ', () => {
+
+    });
+
+    it('must display an error when "password" and "confirm password" values dont match', () => {
+
+    });
+  });
+
+  describe('functional requirements for "email" field', () => {
+    it('must display an error when typing an value and removing it completely ', () => {
+
+    });
+
+    it('must display an error when providing an invalid type of email', () => {
+
+    });
+  });
+
+  describe('functional requirements for "password" field', () => {
+    it('must display an error when typing an value and removing it completely ', () => {
+
+    });
+
+    it('must display an error when providing an value that is not strong enough', () => {
+
+    });
+  });
+
+  describe('functional requirements for "username" field', () => {
+    it('must display an error when typing an value and removing it completely ', () => {
+
+    });
+
+    it('must display an error when providing username that is too long', () => {
+
+    });
+  });
+
+  describe('functional requirements for the "page form"', () => {
+    it('must disable all fields while submitting', () => {
+      // TODO: this even needs implementation.
+    });
+
+    it('must show the "loading spinner" on the "action button" while submitting', () => {
+
+    });
+
+    // TODO: continue writing the test cases (like "show the signup successful page" etc).
+    it('', () => {
+
+    });
+  });
+
+  describe('[method] submit', () => {
+    it('must call the "onSubmit" prop function with "user" as a parameter', () => {
+
+    });
   });
 
   describe('[getter] form.fields', () => {
@@ -27,78 +134,5 @@ describe('[component] SignupPage', () => {
     it('must map the form fields to an object which corresponds to an "user" schema expected by the backend', () => {
 
     });
-  });
-
-  // TODO: See if it is more productive to test the method or the interations on the page.
-  describe('[method] hideErrorPage', () => {
-    it('must hide the "error page", restoring the "signup page" to its default state', () => {
-
-    });
-  });
-
-  // TODO: See if it is more productive to test the method or the interations on the page.
-  describe('[method] resetConfirmPassword', () => {
-    it('must restore the "confirm password" field to its default if both "password" and "confirm password" field\'s values match', () => {
-
-    });
-
-    it('must not restore the "confirm password" field to its default if "password" and "confirm password" field\'s values dont match', () => {
-
-    });
-  });
-
-  // TODO: See if it is more productive to test the method or the interations on the page.
-  describe('[method] setErrorToField', () => {
-    it('must set an "error message" to the given field', () => {
-
-    });
-
-    it('must set the given field as not "pristine" (meaning that the field value has changed)', () => {
-
-    });
-
-    it('must fire a "validation callback" if provided', () => {
-
-    });
-  });
-
-  // TODO: See if it is more productive to test the method or the interations on the page.
-  describe('[method] setErrorToPage', () => {
-    it('must set an error message to be displayed on the page', () => {
-
-    });
-
-    it('must mark the page as "not submitting" because this method is fired after submiting the form', () => {
-
-    });
-
-    it('must give the option to "retry" the form submission again', () => {
-
-    });
-  });
-
-  // TODO: See if it is more productive to test the method or the interations on the page.
-  describe('[method] setValueToField', () => {
-    it('must map the given "event.target.value" as a "field" value', () => {
-
-    });
-  });
-
-  // TODO: See if it is more productive to test the method or the interations on the page.
-  // describe('[method] submit', () => {
-  //   it('must initially mark the page as having no errors and on submitting state', () => {
-
-  //   });
-  // });
-
-  // TODO: See if it is more productive to test the method or the interations on the page.
-  // describe('[method] validateField', () => {
-  //   it('must map the given "event.target.value" as a "field" value', () => {
-
-  //   });
-  // });
-
-  describe('[interaction]', () => {
-
   });
 });
